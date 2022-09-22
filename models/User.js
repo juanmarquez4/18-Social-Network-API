dc2b72ce4bdd418cb10a585d4cd6a2a491b3b2b7
Juan Marquez <juanmarquez4@gmail.com>
2022-09-22 11:44:16 +1000

    Update User model

--- models/User.js
+++ models/User.js
@@ -1,29 +1,47 @@
 const { Schema, model } = require('mongoose');
+const validator = require('validator')
 
 const userSchema = new Schema(
     {
         username: {
             type: String,
-            // unique
+            unique: true,
             required: true,
-            // trimmed
+            trim: true
         },
         email: {
             type: String,
             required: true,
-            // Unique
-            // Must match a valid email address (look into Mongoose's matching validation)
+            unique: true,
+            validate:{
+                validator: validator.isEmail,
+                message: '{VALUE} is not a valid email',
+                isAsync: false
+              }
         },
-        thoughts: {
+        thoughts: [{
+           type: Schema.Types.ObjectId,
+           ref: 'Thought'
             // Array of _id values referencing the Thought model
-        },
-        friends: {
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
             // Array of _id values referencing the User model (self-reference)
-        }
+        }]
+    },
+    {
+        toJason: {
+            virtuals: true,
+        },
+        id: false,
+        
     }
 );
 
-// Schema Settings: Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+            // Schema Settings: Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema
+    .virtual('friendCount')
 
 const User = model('user', userSchema);
 
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController.js');
+
+router.route('/').get(getUsers).post(createUser);
+
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+moduler.exports = router;
